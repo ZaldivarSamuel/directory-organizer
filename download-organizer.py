7b8c08440e86b41836b770389660e2973914e524
Samuel Zaldivar <samuel@Samuels-MacBook-Pro.local>
2021-07-04 20:01:06 -0400

    Added .gitignore and dependency requirements

--- download-organizer.py
+++ download-organizer.py
@@ -1,5 +1,14 @@
-from os import listdir
-from os.path import isfile, join
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
-my_path = "/Users/samuel/Downloads"
-print(listdir(my_path))
+path = "/Users/samuel/Downloads"
+
+if __name__ == "__main__":
+    patterns = ["*"]
+    ignore_patterns = None
+    ignore_directories = True
+    case_sensitive = True
+
+    event_handler = PatternMatchingEventHandler(patterns, ignore_patterns,
+                    ignore_directories, case_sensitive)
