fbf4815b77050b262998e62bba34a23abe33922c
Samuel Zaldivar <samzaldivar98@gmail.com>
2022-04-10 20:24:14 -0400

    Program creates folder for each extension, and moves correct files to each new folder

--- download-organizer.py
+++ download-organizer.py
@@ -1,11 +1,27 @@
+import os
 from os import listdir
 from os.path import isfile, join
+import shutil
+
+# Get file extension for each file.
+# Create a new folder for that extension if not already created
+# Move file to that folder
 
 path = "/Users/samuel/Documents/test"
+folder_set = set()
 
 for f in listdir(path):
     if isfile(join(path, f)):
-        print("File: " + f)
 
+        extension_index = f.rfind('.')
+        extension = f[extension_index + 1:]
 
-#For testing
+        if extension not in folder_set:
+            folder_set.add(extension)
+            folder_path = path + "/" + extension
+            os.mkdir(folder_path)
+
+        original_path = path + "/" + f
+        new_path = path + "/" + extension + "/" + f
+
+        shutil.move(original_path, new_path)
